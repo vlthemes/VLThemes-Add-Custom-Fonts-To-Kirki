8b6aa3b4747617e75d630afcc8fe142348ec3973
vladlent <plafon89@gmail.com>
2017-06-07 21:41:58 +0300

    Some improvement

--- ClassVlthemesAddTypekit.php
+++ ClassVlthemesAddTypekit.php
@@ -1,94 +1,67 @@
+<?php
+
+
+
 /*
  * Add Typekit Fonts
  * */
 
 if(!class_exists('VlthemesAddTypekit')){
 
-	class VlthemesAddTypekit {
+    class VlthemesAddTypekit {
 
-		private $_typekit_id;
-		public $typekit_enable;
-		public $typekit_fonts;
-		public static $fonts_sep = ', ';
-		public static $typekit_subfont = '"Helvetica Neue", Helvetica, Roboto, Arial, sans-serif';
+        private $_typekit_id;
+        public $typekit_enable;
+        public $typekit_fonts;
+        public static $fonts_sep = ', ';
+        public static $typekit_subfont = '"Helvetica Neue", Helvetica, Roboto, Arial, sans-serif';
 
-		public function __construct() {
+        public function __construct() {
 
-			$this->_typekit_id = Kirki::get_option('dreamy_customize', 'typekit_id');
-			$this->typekit_enable = Kirki::get_option('dreamy_customize', 'enable_typekit');
-			$this->typekit_fonts = Kirki::get_option('dreamy_customize', 'typekit_fonts');
-			add_action('wp_head', array($this, 'load_typekit'), 0);
-			add_filter('kirki/fonts/google_fonts', array($this, 'typekit_add_to_kirki'));
+            $this->_typekit_id = bershka_get_option('typekit_id');
+            $this->typekit_enable = bershka_get_option('enable_typekit');
+            $this->typekit_fonts = bershka_get_option('typekit_fonts');
+            add_action('wp_head', array($this, 'load_typekit'), 0);
+            add_filter('kirki/fonts/standard_fonts', array($this, 'typekit_add_to_kirki'));
 
-		}
+        }
 
-		public function load_typekit() {
+        public function load_typekit() {
 
-			if (!empty($this->_typekit_id) && $this->typekit_enable){
-				echo '<script src="//use.typekit.net/'.esc_attr($this->sanitize_typekit_id($this->_typekit_id)) .'.js"></script>';
-				echo '<script>try{Typekit.load({ async: true });}catch(e){}</script>';
-			}
+            if (!empty($this->_typekit_id) && $this->typekit_enable){
+                echo '<script src="//use.typekit.net/'.esc_attr($this->sanitize_typekit_id($this->_typekit_id)) .'.js"></script>';
+                echo '<script>try{Typekit.load({ async: true });}catch(e){}</script>';
+            }
 
-		}
+        }
 
-		public function sanitize_typekit_id($id) {
-			return preg_replace('/[^0-9a-z]+/', '', $id);
-		}
+        public function sanitize_typekit_id($id) {
+            return preg_replace('/[^0-9a-z]+/', '', $id);
+        }
 
 
-		public function typekit_add_to_kirki($google_fonts){
+        public function typekit_add_to_kirki($standard_fonts){
 
-			$my_custom_fonts = array();
 
-			if($this->typekit_enable && !empty($this->_typekit_id)){
+            if($this->typekit_enable && !empty($this->_typekit_id && $this->typekit_fonts)){
 
-				$typekit_fonts = trim($this->typekit_fonts, ' ');
-				$typekit_fonts = explode(',', $typekit_fonts);
 
-				foreach($typekit_fonts as $font => $value) {
-					$my_custom_fonts[$value] = array(
-						'label' => $value,
-						'variants' => array(
-							'100',
-							'100italic',
-							'200',
-							'200italic',
-							'300',
-							'300italic',
-							'regular',
-							'italic',
-							'500',
-							'500italic',
-							'600',
-							'600italic',
-							'700',
-							'700italic',
-							'800',
-							'800italic',
-							'900',
-							'900italic',
-						),
-						'subsets' => array(
-							'cyrillic' => 'Cyrillic',
-							'cyrillic-ext' => 'Cyrillic Extended',
-							'devanagari' => 'Devanagari',
-							'greek' => 'Greek',
-							'greek-ext' => 'Greek Extended',
-							'khmer' => 'Khmer',
-							'latin' => 'Latin',
-						),
-						'subfonts' => self::$typekit_subfont,
-						'category' => 'sans-serif',
-					);
-				}
+                foreach($this->typekit_fonts as $key=>$typekit_font) :
 
-			}
+                    $standard_fonts[$typekit_font['font_css_name']] = array(
+                        'label' => $typekit_font['font_name'],
+                        'stack' => $typekit_font['font_css_name'] . ', '. $typekit_font['font_fallback'].',""'
+                    );
 
-			return array_merge_recursive($my_custom_fonts, $google_fonts);
+                endforeach;
 
-		}
+            }
 
-	}
+            return $standard_fonts;
+
+        }
+
+    }
 }
 
 new VlthemesAddTypekit;
--- section-typekit.php
+++ section-typekit.php
@@ -6,21 +6,21 @@
 
 $priority = 0;
 
-Kirki::add_field( 'dreamy_customize', array(
+Kirki::add_field( 'bershka_customize', array(
 	'type' => 'checkbox',
 	'settings' => 'enable_typekit',
-	'label' => esc_html__('Enable Typekit', 'dreamy'),
+	'label' => esc_html__('Enable Typekit', 'bershka'),
 	'section' => 'section_typekit',
-	'default' => '0',
+	'default' => '1',
 	'priority' => $priority++,
 ) );
 
-Kirki::add_field( 'dreamy_customize', array(
+Kirki::add_field( 'bershka_customize', array(
 	'type' => 'text',
 	'settings' => 'typekit_id',
-	'label' => esc_html__('Typekit ID', 'dreamy'),
+	'label' => esc_html__('Typekit ID', 'bershka'),
 	'section' => 'section_typekit',
-	'default' => '',
+	'default' => 'jrw6bgj',
 	'priority' => $priority++,
 	'transport' => 'postMessage',
 	'required' => array(
@@ -33,20 +33,53 @@ Kirki::add_field( 'dreamy_customize', array(
 ) );
 
 
-Kirki::add_field( 'dreamy_customize', array(
-	'type' => 'text',
-	'settings' => 'typekit_fonts',
-	'label' => esc_html__('Typekit Fonts', 'dreamy'),
-	'description' => esc_html__('Enter your typekit fonts separate with coma', 'dreamy'),
-	'section' => 'section_typekit',
-	'default' => '',
-	'priority' => $priority++,
-	'transport' => 'postMessage',
-	'required' => array(
-		array(
-			'setting' => 'enable_typekit',
-			'operator' => '==',
-			'value' => '1',
-		)
-	),
-) );
+
+Kirki::add_field('bershka_customize', array(
+    'type' => 'repeater',
+    'label' => esc_html__('Typekit Fonts', 'bershka'),
+    'description' => esc_html__('Here you can add typekit fonts', 'bershka'),
+    'settings' => 'typekit_fonts',
+    'priority' => $priority++,
+    'section' => 'section_typekit',
+    'row_label' => array(
+        'type' => 'text',
+        'value' => esc_html__('Font', 'bershka'),
+    ),
+    'default' => array(
+        array(
+            'font_name' => 'Essones',
+            'font_css_name' => 'essonnes-text',
+            'font_fallback' => 'serif',
+        ),
+        array(
+            'font_name' => 'Europa',
+            'font_css_name' => 'europa',
+            'font_fallback' => 'sans-serif',
+        )
+    ),
+    'fields' => array(
+        'font_name' => array(
+            'type' => 'text',
+            'label' => esc_html__('Name', 'bershka'),
+        ),
+        'font_css_name' => array(
+            'type' => 'text',
+            'label' => esc_html__('CSS Name', 'bershka'),
+        ),
+        'font_fallback' => array(
+            'type' => 'select',
+            'label' => esc_html__('Fallback', 'bershka'),
+            'choices' => array(
+                'sans-serif' => esc_html__('Sans Serif', 'bershka'),
+                'serif' => esc_html__('Serif', 'bershka'),
+            )
+        )
+    ),
+    'active_callback' => array(
+        array(
+            'setting' => 'enable_typekit',
+            'operator' => '==',
+            'value' => '1'
+        )
+    )
+));
