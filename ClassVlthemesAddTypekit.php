330e066668ed62850c47da9ad24f83c0f952e38c
vladlent <plafon89@gmail.com>
2017-06-26 09:39:58 +0300

    fix conflicts

--- ClassVlthemesAddTypekit.php
+++ ClassVlthemesAddTypekit.php
@@ -4,18 +4,16 @@
  * Add Typekit Fonts
  * */
 
-
 class VlthemesAddTypekit {
     private $_typekit_id;
     public $typekit_enable;
     public $typekit_fonts;
-    public static $fonts_sep = ', ';
     public function __construct() {
         $this->_typekit_id = osmos_get_option('typekit_id');
         $this->typekit_enable = osmos_get_option('enable_typekit');
         $this->typekit_fonts = osmos_get_option('typekit_fonts');
         add_action('wp_head', array($this, 'load_typekit'), 0);
-        add_filter('kirki/fonts/google_fonts', array($this, 'typekit_add_to_kirki'));
+        add_filter('kirki/fonts/google_fonts', array($this, 'add_typekit_fonts'));
     }
     public function load_typekit() {
         if (!empty($this->_typekit_id) && $this->typekit_enable){
@@ -26,34 +24,14 @@ class VlthemesAddTypekit {
     public function sanitize_typekit_id($id) {
         return preg_replace('/[^0-9a-z]+/', '', $id);
     }
-    public function typekit_add_to_kirki($google_fonts){
+    public function add_typekit_fonts($google_fonts){
         $my_custom_fonts = array();
         if($this->typekit_enable && !empty($this->_typekit_id && $this->typekit_fonts)){
-
             foreach($this->typekit_fonts as $key=>$typekit_font){
-
                 $my_custom_fonts[$typekit_font['font_css_name']] = array(
                     'label' => $typekit_font['font_name'],
-                    'variants' => array(
-                        '100',
-                        '100italic',
-                        '200',
-                        '200italic',
-                        '300',
-                        '300italic',
-                        'regular',
-                        'italic',
-                        '500',
-                        '500italic',
-                        '600',
-                        '600italic',
-                        '700',
-                        '700italic',
-                        '800',
-                        '800italic',
-                        '900',
-                        '900italic',
-                    ),
+                    'variants' => $typekit_font['font_variants'],
+                    'subsets'  => $typekit_font['font_subsets'],
                     'category' => $typekit_font['font_fallback']
                 );
             }
--- section-typekit.php
+++ section-typekit.php
@@ -1,80 +1,122 @@
 <?php
 
 /*
- * Typekit Settings
- * */
+* Typekit Settings
+* */
 
 $priority = 0;
 
-Kirki::add_field( 'bershka_customize', array(
-	'type' => 'checkbox',
-	'settings' => 'enable_typekit',
-	'label' => esc_html__('Enable Typekit', 'bershka'),
-	'section' => 'section_typekit',
-	'default' => '1',
-	'priority' => $priority++,
-) );
+Kirki::add_field('osmos_customize', array(
+    'type' => 'switch',
+    'settings' => 'enable_typekit',
+    'label' => esc_html__('Enable Typekit', 'osmos') ,
+    'section' => 'section_typekit',
+    'default' => '1',
+    'priority' => $priority++,
+    'transport' => 'auto',
+    'choices' => array(
+        'on'  => esc_html__('Enable', 'osmos'),
+        'off' => esc_html__('Disable', 'osmos')
+    )
+));
 
-Kirki::add_field( 'bershka_customize', array(
-	'type' => 'text',
-	'settings' => 'typekit_id',
-	'label' => esc_html__('Typekit ID', 'bershka'),
-	'section' => 'section_typekit',
-	'default' => 'jrw6bgj',
-	'priority' => $priority++,
-	'transport' => 'postMessage',
-	'required' => array(
-		array(
-			'setting' => 'enable_typekit',
-			'operator' => '==',
-			'value' => '1',
-		)
-	),
-) );
-
-
-
-Kirki::add_field('bershka_customize', array(
+Kirki::add_field('osmos_customize', array(
+    'type' => 'text',
+    'settings' => 'typekit_id',
+    'label' => esc_html__('Typekit ID', 'osmos') ,
+    'section' => 'section_typekit',
+    'default' => 'fyf1uam',
+    'priority' => $priority++,
+    'transport' => 'auto',
+    'required' => array(
+        array(
+            'setting' => 'enable_typekit',
+            'operator' => '==',
+            'value' => '1',
+        )
+    ) ,
+));
+Kirki::add_field('osmos_customize', array(
     'type' => 'repeater',
-    'label' => esc_html__('Typekit Fonts', 'bershka'),
-    'description' => esc_html__('Here you can add typekit fonts', 'bershka'),
+    'label' => esc_html__('Typekit Fonts', 'osmos') ,
+    'description' => esc_html__('Here you can add typekit fonts', 'osmos') ,
     'settings' => 'typekit_fonts',
     'priority' => $priority++,
+    'transport' => 'auto',
     'section' => 'section_typekit',
     'row_label' => array(
         'type' => 'text',
-        'value' => esc_html__('Font', 'bershka'),
+        'value' => esc_html__('Typekit Font', 'osmos') ,
     ),
     'default' => array(
         array(
-            'font_name' => 'Essones',
-            'font_css_name' => 'essonnes-text',
-            'font_fallback' => 'serif',
-        ),
-        array(
-            'font_name' => 'Europa',
-            'font_css_name' => 'europa',
+            'font_name' => 'Sofia Pro',
+            'font_css_name' => 'sofia-pro',
             'font_fallback' => 'sans-serif',
-        )
+            'font_subsets' => 'cyrillic',
+            'font_variants' => array('300', '300italic', 'regular', 'italic', '500', '700')
+        ),
     ),
     'fields' => array(
         'font_name' => array(
             'type' => 'text',
-            'label' => esc_html__('Name', 'bershka'),
-        ),
+            'label' => esc_html__('Name', 'osmos') ,
+        ) ,
         'font_css_name' => array(
             'type' => 'text',
-            'label' => esc_html__('CSS Name', 'bershka'),
+            'label' => esc_html__('CSS Name', 'osmos') ,
+        ) ,
+        'font_variants' => array(
+            'type' => 'select',
+            'label' => esc_html__('Variants', 'osmos') ,
+            'multiple' => 18,
+            'choices' => array(
+                '100' => esc_html__('100', 'osmos') ,
+                '100italic' => esc_html__('100italic', 'osmos') ,
+                '200' => esc_html__('200', 'osmos') ,
+                '200italic' => esc_html__('200italic', 'osmos') ,
+                '300' => esc_html__('300', 'osmos') ,
+                '300italic' => esc_html__('300italic', 'osmos') ,
+                'regular' => esc_html__('regular', 'osmos') ,
+                'italic' => esc_html__('italic', 'osmos') ,
+                '500' => esc_html__('500', 'osmos') ,
+                '500italic' => esc_html__('500italic', 'osmos') ,
+                '600' => esc_html__('600', 'osmos') ,
+                '600italic' => esc_html__('600italic', 'osmos') ,
+                '700' => esc_html__('700', 'osmos') ,
+                '700italic' => esc_html__('700italic', 'osmos') ,
+                '800' => esc_html__('800', 'osmos') ,
+                '800italic' => esc_html__('800italic', 'osmos') ,
+                '900' => esc_html__('900', 'osmos') ,
+                '900italic' => esc_html__('900italic', 'osmos') ,
+            )
         ),
         'font_fallback' => array(
             'type' => 'select',
-            'label' => esc_html__('Fallback', 'bershka'),
+            'label' => esc_html__('Fallback', 'osmos') ,
             'choices' => array(
-                'sans-serif' => esc_html__('Sans Serif', 'bershka'),
-                'serif' => esc_html__('Serif', 'bershka'),
+                'sans-serif' => esc_html__('Helvetica, Arial, sans-serif', 'osmos') ,
+                'serif' => esc_html__('Georgia, serif', 'osmos') ,
+                'display' => esc_html__('"Comic Sans MS", cursive, sans-serif', 'osmos') ,
+                'handwriting' => esc_html__('"Comic Sans MS", cursive, sans-serif', 'osmos') ,
+                'monospace' => esc_html__('"Lucida Console", Monaco, monospace', 'osmos') ,
             )
-        )
-    ),
+        ) ,
+        'font_subsets' => array(
+            'type' => 'select',
+            'label' => esc_html__('Subsets', 'osmos') ,
+            'multiple' => 7,
+            'choices' => array(
+                'cyrillic' => esc_html__('Cyrillic', 'osmos') ,
+                'cyrillic-ext' => esc_html__('Cyrillic Extended', 'osmos') ,
+                'devanagari' => esc_html__('Devanagari', 'osmos') ,
+                'greek' => esc_html__('Greek', 'osmos') ,
+                'greek-ext' => esc_html__('Greek Extended', 'osmos') ,
+                'khmer' => esc_html__('Khmer', 'osmos') ,
+                'latin' => esc_html__('Latin', 'osmos') ,
+            )
+        ) ,
+    ) ,
     'active_callback' => array(
         array(
             'setting' => 'enable_typekit',
@@ -83,5 +125,4 @@ Kirki::add_field('bershka_customize', array(
         )
     )
 ));
-
 
