63e0f492ec1b7a773fcb4e3a03fdc0daa9614ffc
vladlent <plafon89@gmail.com>
2017-09-06 19:33:40 +0300

    fixed some issues

--- ClassVlthemesAddTypekit.php
+++ ClassVlthemesAddTypekit.php
@@ -4,35 +4,34 @@
  * Add Typekit Fonts
  * */
 
-class VlthemesAddTypekit {
+class themeAddTypekit {
     private $_typekit_id;
     public $typekit_enable;
     public $typekit_fonts;
     public function __construct() {
-        $this->_typekit_id = osmos_get_option('typekit_id');
-        $this->typekit_enable = osmos_get_option('enable_typekit');
-        $this->typekit_fonts = osmos_get_option('typekit_fonts');
-        add_action('wp_head', array($this, 'load_typekit'), 0);
-        add_filter('kirki/fonts/google_fonts', array($this, 'add_typekit_fonts'));
+        $this->_typekit_id = ramsay_get_option( 'typekit_id' );
+        $this->typekit_enable = ramsay_get_option( 'enable_typekit' );
+        $this->typekit_fonts = ramsay_get_option( 'typekit_fonts' );
+        add_action( 'wp_head', array( $this, 'load_typekit' ), 0 );
+        add_filter( 'kirki/fonts/google_fonts', array( $this, 'add_typekit_fonts' ) );
     }
     public function load_typekit() {
-        if (!empty($this->_typekit_id) && $this->typekit_enable){
-            echo '<script src="https://use.typekit.net/'.esc_attr($this->sanitize_typekit_id($this->_typekit_id)) .'.js"></script>';
-            echo '<script>try{Typekit.load({ async: true });}catch(e){}</script>';
+        if ( ! empty( $this->_typekit_id ) && $this->typekit_enable ){
+            echo '<script src="https://use.typekit.net/'.esc_attr( $this->sanitize_typekit_id( $this->_typekit_id ) ) .'.js"></script>';
         }
     }
-    public function sanitize_typekit_id($id) {
-        return preg_replace('/[^0-9a-z]+/', '', $id);
+    public function sanitize_typekit_id( $id ) {
+        return preg_replace( '/[^0-9a-z]+/', '', $id );
     }
-    public function add_typekit_fonts($google_fonts){
+    public function add_typekit_fonts( $google_fonts ){
         $my_custom_fonts = array();
-        if($this->typekit_enable && !empty($this->_typekit_id && $this->typekit_fonts)){
-            foreach($this->typekit_fonts as $key=>$typekit_font){
-                $my_custom_fonts[$typekit_font['font_css_name']] = array(
-                    'label' => $typekit_font['font_name'],
-                    'variants' => $typekit_font['font_variants'],
-                    'subsets'  => $typekit_font['font_subsets'],
-                    'category' => $typekit_font['font_fallback']
+        if ( $this->typekit_enable && !empty( $this->_typekit_id ) && ! empty( $this->typekit_fonts ) ) {
+            foreach( $this->typekit_fonts as $key=>$typekit_font ){
+                $my_custom_fonts[ $typekit_font[ 'font_css_name' ] ] = array(
+                    'label' => $typekit_font[ 'font_name' ],
+                    'variants' => $typekit_font[ 'font_variants' ],
+                    'subsets'  => $typekit_font[ 'font_subsets' ],
+                    'category' => $typekit_font[ 'font_fallback' ]
                 );
             }
         }
@@ -40,5 +39,4 @@ class VlthemesAddTypekit {
     }
 }
 
-new VlthemesAddTypekit;
-
+new themeAddTypekit;
--- section-typekit.php
+++ section-typekit.php
@@ -1,31 +1,31 @@
 <?php
 
-/*
-* Typekit Settings
-* */
+/**
+ * Typekit
+ */
 
 $priority = 0;
 
-Kirki::add_field('osmos_customize', array(
+Kirki::add_field( 'ramsay_customize', array(
     'type' => 'switch',
     'settings' => 'enable_typekit',
-    'label' => esc_html__('Enable Typekit', 'osmos') ,
+    'label' => esc_html__( 'Enable Typekit', 'ramsay' ) ,
     'section' => 'section_typekit',
     'default' => '1',
     'priority' => $priority++,
     'transport' => 'auto',
     'choices' => array(
-        'on'  => esc_html__('Enable', 'osmos'),
-        'off' => esc_html__('Disable', 'osmos')
+        'on'  => esc_html__( 'Enable', 'ramsay' ),
+        'off' => esc_html__( 'Disable', 'ramsay' )
     )
-));
+) );
 
-Kirki::add_field('osmos_customize', array(
+Kirki::add_field( 'ramsay_customize', array(
     'type' => 'text',
     'settings' => 'typekit_id',
-    'label' => esc_html__('Typekit ID', 'osmos') ,
+    'label' => esc_html__( 'Typekit ID', 'ramsay' ) ,
     'section' => 'section_typekit',
-    'default' => 'fyf1uam',
+    'default' => 'uex0khw',
     'priority' => $priority++,
     'transport' => 'auto',
     'required' => array(
@@ -35,85 +35,86 @@ Kirki::add_field('osmos_customize', array(
             'value' => '1',
         )
     ) ,
-));
-Kirki::add_field('osmos_customize', array(
+) );
+
+Kirki::add_field( 'ramsay_customize', array(
     'type' => 'repeater',
-    'label' => esc_html__('Typekit Fonts', 'osmos') ,
-    'description' => esc_html__('Here you can add typekit fonts', 'osmos') ,
+    'label' => esc_html__( 'Typekit Fonts', 'ramsay' ) ,
+    'description' => esc_html__( 'Here you can add typekit fonts', 'ramsay' ) ,
     'settings' => 'typekit_fonts',
     'priority' => $priority++,
     'transport' => 'auto',
     'section' => 'section_typekit',
     'row_label' => array(
         'type' => 'text',
-        'value' => esc_html__('Typekit Font', 'osmos') ,
+        'value' => esc_html__( 'Typekit Font', 'ramsay' ) ,
     ),
     'default' => array(
         array(
-            'font_name' => 'Sofia Pro',
-            'font_css_name' => 'sofia-pro',
+            'font_name' => 'Azo Sans',
+            'font_css_name' => 'azo-sans-web',
+            'font_variants' => array( 'regular', 'italic', '700', '700italic' ),
             'font_fallback' => 'sans-serif',
-            'font_subsets' => 'cyrillic',
-            'font_variants' => array('300', '300italic', 'regular', 'italic', '500', '700')
-        ),
+            'font_subsets' => 'latin'
+        )
     ),
     'fields' => array(
         'font_name' => array(
             'type' => 'text',
-            'label' => esc_html__('Name', 'osmos') ,
+            'label' => esc_html__( 'Name', 'ramsay' ) ,
         ) ,
         'font_css_name' => array(
             'type' => 'text',
-            'label' => esc_html__('CSS Name', 'osmos') ,
+            'label' => esc_html__( 'CSS Name', 'ramsay' ) ,
         ) ,
         'font_variants' => array(
             'type' => 'select',
-            'label' => esc_html__('Variants', 'osmos') ,
+            'label' => esc_html__( 'Variants', 'ramsay' ) ,
             'multiple' => 18,
             'choices' => array(
-                '100' => esc_html__('100', 'osmos') ,
-                '100italic' => esc_html__('100italic', 'osmos') ,
-                '200' => esc_html__('200', 'osmos') ,
-                '200italic' => esc_html__('200italic', 'osmos') ,
-                '300' => esc_html__('300', 'osmos') ,
-                '300italic' => esc_html__('300italic', 'osmos') ,
-                'regular' => esc_html__('regular', 'osmos') ,
-                'italic' => esc_html__('italic', 'osmos') ,
-                '500' => esc_html__('500', 'osmos') ,
-                '500italic' => esc_html__('500italic', 'osmos') ,
-                '600' => esc_html__('600', 'osmos') ,
-                '600italic' => esc_html__('600italic', 'osmos') ,
-                '700' => esc_html__('700', 'osmos') ,
-                '700italic' => esc_html__('700italic', 'osmos') ,
-                '800' => esc_html__('800', 'osmos') ,
-                '800italic' => esc_html__('800italic', 'osmos') ,
-                '900' => esc_html__('900', 'osmos') ,
-                '900italic' => esc_html__('900italic', 'osmos') ,
+                '100' => esc_html__( '100', 'ramsay' ) ,
+                '100italic' => esc_html__( '100italic', 'ramsay' ) ,
+                '200' => esc_html__( '200', 'ramsay' ) ,
+                '200italic' => esc_html__( '200italic', 'ramsay' ) ,
+                '300' => esc_html__( '300', 'ramsay' ) ,
+                '300italic' => esc_html__( '300italic', 'ramsay' ) ,
+                'regular' => esc_html__( 'regular', 'ramsay' ) ,
+                'italic' => esc_html__( 'italic', 'ramsay' ) ,
+                '500' => esc_html__( '500', 'ramsay' ) ,
+                '500italic' => esc_html__( '500italic', 'ramsay' ) ,
+                '600' => esc_html__( '600', 'ramsay' ) ,
+                '600italic' => esc_html__( '600italic', 'ramsay' ) ,
+                '700' => esc_html__( '700', 'ramsay' ) ,
+                '700italic' => esc_html__( '700italic', 'ramsay' ) ,
+                '800' => esc_html__( '800', 'ramsay' ) ,
+                '800italic' => esc_html__( '800italic', 'ramsay' ) ,
+                '900' => esc_html__( '900', 'ramsay' ) ,
+                '900italic' => esc_html__( '900italic', 'ramsay' ) ,
             )
         ),
         'font_fallback' => array(
             'type' => 'select',
-            'label' => esc_html__('Fallback', 'osmos') ,
+            'label' => esc_html__( 'Fallback', 'ramsay' ) ,
             'choices' => array(
-                'sans-serif' => esc_html__('Helvetica, Arial, sans-serif', 'osmos') ,
-                'serif' => esc_html__('Georgia, serif', 'osmos') ,
-                'display' => esc_html__('"Comic Sans MS", cursive, sans-serif', 'osmos') ,
-                'handwriting' => esc_html__('"Comic Sans MS", cursive, sans-serif', 'osmos') ,
-                'monospace' => esc_html__('"Lucida Console", Monaco, monospace', 'osmos') ,
+                'sans-serif' => esc_html__( 'Helvetica, Arial, sans-serif', 'ramsay' ) ,
+                'serif' => esc_html__( 'Georgia, serif', 'ramsay' ) ,
+                'display' => esc_html__( '"Comic Sans MS", cursive, sans-serif', 'ramsay' ) ,
+                'handwriting' => esc_html__( '"Comic Sans MS", cursive, sans-serif', 'ramsay' ) ,
+                'monospace' => esc_html__( '"Lucida Console", Monaco, monospace', 'ramsay' ) ,
             )
         ) ,
         'font_subsets' => array(
             'type' => 'select',
-            'label' => esc_html__('Subsets', 'osmos') ,
+            'label' => esc_html__( 'Subsets', 'ramsay' ) ,
             'multiple' => 7,
             'choices' => array(
-                'cyrillic' => esc_html__('Cyrillic', 'osmos') ,
-                'cyrillic-ext' => esc_html__('Cyrillic Extended', 'osmos') ,
-                'devanagari' => esc_html__('Devanagari', 'osmos') ,
-                'greek' => esc_html__('Greek', 'osmos') ,
-                'greek-ext' => esc_html__('Greek Extended', 'osmos') ,
-                'khmer' => esc_html__('Khmer', 'osmos') ,
-                'latin' => esc_html__('Latin', 'osmos') ,
+                'cyrillic' => esc_html__( 'Cyrillic', 'ramsay' ) ,
+                'cyrillic-ext' => esc_html__( 'Cyrillic Extended', 'ramsay' ) ,
+                'devanagari' => esc_html__( 'Devanagari', 'ramsay' ) ,
+                'greek' => esc_html__( 'Greek', 'ramsay' ) ,
+                'greek-ext' => esc_html__( 'Greek Extended', 'ramsay' ) ,
+                'khmer' => esc_html__( 'Khmer', 'ramsay' ) ,
+                'latin' => esc_html__( 'Latin', 'ramsay' ) ,
             )
         ) ,
     ) ,
@@ -124,5 +125,4 @@ Kirki::add_field('osmos_customize', array(
             'value' => '1'
         )
     )
-));
-
+) );
